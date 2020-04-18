85d3e97386075ae480653c4d1277a0b45fc5b4b1
Luca Paganin <luca.paganin@ge.infn.it>
2020-04-18 22:35:51 +0200

    first functions

--- data/data_gen.py
+++ data/data_gen.py
@@ -1,3 +1,16 @@
-import os
 import numpy as np
-import matplotlib.pyplot as plt
+
+def get_time_domain(ts, delta, sampling):
+    return np.linspace(0, ts*delta, num=ts*sampling)
+
+def get_means(tmax):
+    return np.random.random(20) * tmax
+
+def get_sigmas(tmax):
+    return np.random.random(20) * tmax/4
+
+def signal(t, means, sigmas):
+    f = np.zeros(t.shape)
+    for mean,sigma in zip(means, sigmas):
+        f += np.exp(-(t-mean)**2/(2*sigma**2))
+    return f
