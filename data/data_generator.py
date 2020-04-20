b64c2dcc718c39df45b1acb1ac5a85437ce10231
Luca Paganin <luca.paganin@ge.infn.it>
2020-04-20 16:08:35 +0200

    First implementation of noise

--- data/data_generator.py
+++ data/data_generator.py
@@ -1,7 +1,9 @@
 import numpy as np
+import pandas as pd
 
 class ParameterError(Exception):
     pass
+
 
 class DataGenerator:
     def __init__(self,
@@ -12,7 +14,8 @@ class DataGenerator:
                  seed=0,
                  n_gaps=None,
                  gap_size=None,
-                 noise_level=0):
+                 noise_amplitude=0,
+                 relative_error_amplitude=None):
         """!
         Constructor of the class
 
@@ -28,9 +31,10 @@ class DataGenerator:
 
         :param n_gaps: number of gaps to simulate in artificial data, positive integer
 
-        :param gap_size: size of simulated gaps, positive integer. It represents the number of missing samples for each gap
+        :param gap_size: size of simulated gaps, positive integer. It represents the number of missing samples for
+        each gap
 
-        :param noise_level: relative size of noise for error bars
+        :param noise_amplitude: relative size of noise wrt to signal
         """
         if ts is None:
             raise ParameterError("Total observational time cannot be None")
@@ -41,19 +45,19 @@ class DataGenerator:
         if samples_per_delta is None:
             raise ParameterError("Sampling frequency cannot be None")
         self.samples_per_delta = samples_per_delta
+        if relative_error_amplitude is None:
+            raise ParameterError("Relative error amplitude cannot be none")
+        self.relative_error_amplitude = relative_error_amplitude
 
         np.random.seed(seed)
         self.tmax = self.ts * self.delta
         self.original_n_samples = self.ts * self.samples_per_delta
         self.t_regular = np.linspace(0, self.tmax, num=self.original_n_samples)
-
-        # Add sampling noise to regularly sampled time domain
-        sampling_noise = (np.random.random(len(self.t_regular)) - 0.5) * (self.delta / self.samples_per_delta)
-        self.t_domain = self.t_regular + sampling_noise
+        self.t_domain = self.t_regular.copy()
 
         self.n_gaps = n_gaps
         self.gap_size = gap_size
-        self.noise_level = noise_level
+        self.noise_amplitude = noise_amplitude
         self.image_ratio = image_ratio
 
         if self.n_gaps is not None:
@@ -62,8 +66,8 @@ class DataGenerator:
         if self.gap_size is not None:
             if self.gap_size < 0 or int(self.gap_size) != self.gap_size:
                 raise ParameterError(f"Gap size parameter must be a positive integer, not {self.gap_size}")
-        if self.noise_level < 0:
-            raise ParameterError("Noise level cannot be negative")
+        if self.noise_amplitude < 0:
+            raise ParameterError("Noise amplitude cannot be negative")
         if self.image_ratio is not None and self.image_ratio < 0:
             raise ParameterError("Image ratio cannot be negative")
         self.gaussian_means = None
@@ -104,14 +108,27 @@ class DataGenerator:
         mask = np.ones(len(self.t_domain), dtype=bool)
         for c in gap_centers:
             # Compute left and right indices around gap
-            a = c - int(gapsize / 2) + (1 - gapsize % 2)
-            b = c + int(gapsize / 2) + 1
+            a = c - int(self.gap_size / 2) + (1 - self.gap_size % 2)
+            b = c + int(self.gap_size / 2) + 1
             mask[a:b] = False
 
         return mask
 
-    def generate_light_curves(self, mask=None, means=None, sigmas=None):
+    def generate_light_curves(self, mask=None, means=None, sigmas=None, noise_amplitude=None):
+        """
+        :param mask, numpy boolean array for masking
+
+        :param means, numpy array containing means of gaussians to be superimposed
+
+        :param sigmas, numpy array containing sigmas of gaussians to be superimposed
+
+        :param noise_amplitude, float representing the fraction of the flux used to compute the sigma of the gaussian
+        noise
+
+        :rtype: tuple containing time_domain, signal a, signal b, errors on a, errors on b
+        """
         time_domain = self.t_domain
+        noise_amplitude = noise_amplitude if noise_amplitude is not None else 0
         if mask is not None:
             time_domain = self.t_domain[mask]
         if means is None or sigmas is None:
@@ -119,8 +136,17 @@ class DataGenerator:
 
         a = self.gaussian_signal(time_domain, means, sigmas)
         b = self.gaussian_signal(time_domain + self.delta, means, sigmas)
-        err_a = np.abs(self.noise_level * a)
-        err_b = np.abs(self.noise_level * b)
+        sigma_a = np.abs(self.relative_error_amplitude * a)
+        sigma_b = np.abs(self.relative_error_amplitude * b)
+        if noise_amplitude > 0:
+            noise_a = np.random.normal(0, noise_amplitude * a)
+            noise_b = np.random.normal(0, noise_amplitude * b)
+            a += noise_a
+            b += noise_b
 
-        return time_domain, a, b, err_a, err_b
+        return time_domain, a, b, sigma_a, sigma_b
 
+    def generate_dataset(self, mask=None, means=None, sigmas=None, noise_amplitude=None):
+        *data, = self.generate_light_curves(mask=mask, means=means, sigmas=sigmas, noise_amplitude=noise_amplitude)
+        df = pd.DataFrame(data=np.array(data).T, columns=["time", "A", "B", "SigmaA", "SigmaB"])
+        return df
