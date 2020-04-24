274fa134d519ab61ae554679f6e232449c521105
Luca Paganin <luca.paganin@ge.infn.it>
2020-04-24 23:36:29 +0200

    Modified interface of class

--- data/data_generator.py
+++ data/data_generator.py
@@ -11,60 +11,62 @@ class ParameterError(Exception):
 class DataGenerator:
     """This is a class which generates datasets of artificial light curves data
 
-    :param ts: total observation time, in unit of delta
-    :type ts: float
-    :param delta: value of the time delay, unit is days
-    :type delta: float
-    :param tmax: total observation time, obtained from the product between ``ts`` and ``delta``
-    :type tmax: float
-    :param samples_per_delta: value of the regular sampling frequency, in units of 1/delta
-    :type samples_per_delta: float
-    :param image_ratio: ratio between the two image signals
-    :type image_ratio: float
+    :param t_max: total observation time, unit is days
+    :type t_max: float
+    :param delay: value of the time delay, unit is days
+    :type delay: float
+    :param n_samples: number of samples
+    :type n_samples: int
     :param seed: seed of numpy random generator
     :type seed: float
     :param n_gaps: number of gaps to simulate in artificial data
     :type n_gaps: int
-    :param gap_size: size of simulated gaps, positive integer. It represents the number of missing samples for
-                     each gap
+    :param gap_size: size of simulated gaps, positive integer. It represents the number of missing samples for each gap
     :type gap_size: int
-    :param noise_level: relative size of error bars wrt to signal
-    :type noise_level: float
+    :param relative_err: relative size of error bars wrt to signal
+    :type relative_err: float
     :param image_ratio: multiplicative factor between the two light curves
     :type image_ratio: float
+    :param add_noise: boolean to decide whether to contaminate or not the signal with gaussian noise
+    :type add_noise: bool
     """
+
     def __init__(self,
-                 ts=None,
-                 delta=None,
-                 samples_per_delta=None,
+                 t_max=None,
+                 delay=None,
+                 n_samples=None,
                  image_ratio=1. / 1.44,
                  seed=0,
                  n_gaps=None,
                  gap_size=None,
-                 noise_level=None):
+                 relative_err=None,
+                 add_noise=False):
 
-        if ts is None:
-            raise ParameterError("Total observational time cannot be None")
-        self.ts = ts
-        if delta is None:
+        if t_max is None or t_max < 0:
+            raise ParameterError("Total observation time cannot be None, negative or zero")
+        self.t_max = t_max
+        if delay is None:
             raise ParameterError("Time delay cannot be None")
-        self.delta = delta
-        if samples_per_delta is None:
-            raise ParameterError("Sampling frequency cannot be None")
-        self.samples_per_delta = samples_per_delta
-        if noise_level is None or noise_level < 0:
-            raise ParameterError("Noise level cannot be none or negative")
-        self.noise_level = noise_level
-        if image_ratio is None or image_ratio < 0:
-            raise ParameterError("Image ratio cannot be None or negative!")
+        self.delay = delay
+        if n_samples is None or n_samples < 0 or n_samples == 0:
+            raise ParameterError("Number of samples cannot be None, negative or zero")
+        try:
+            self.n_samples = int(n_samples)
+        except:
+            raise Exception(f"Cannot cast variable {n_samples} as an integer")
+        if relative_err is None or relative_err < 0:
+            raise ParameterError("Relative error cannot be none or negative")
+        self.relative_err = relative_err
+        if image_ratio is None or image_ratio < 0 or image_ratio == 0:
+            raise ParameterError("Image ratio cannot be None, negative or zero")
         self.image_ratio = image_ratio
 
         np.random.seed(seed)
-        self.tmax = self.ts * self.delta
-        self.t_regular = np.linspace(0, self.tmax, num=int(self.ts * self.samples_per_delta))
+        self.t_regular = np.linspace(0, self.t_max, num=self.n_samples)
         self.t_domain = self.t_regular.copy()
         self.n_gaps = n_gaps
         self.gap_size = gap_size
+        self.add_noise = add_noise
 
         if self.n_gaps is not None:
             if self.n_gaps < 0 or int(self.n_gaps) != self.n_gaps:
@@ -77,17 +79,17 @@ class DataGenerator:
         self.gaussian_peak_heights = None
 
     def generate_gaussian_signal_parameters(self):
-        means = np.random.random(20) * self.tmax
-        sigmas = np.random.random(20) * (self.tmax / 4)
-        # heights = 1 + 5 * np.random.random(20)
-        heights = np.ones(means.shape)
+        means = np.random.random(20) * self.t_max
+        sigmas = np.random.random(20) * (self.t_max / 4)
+        heights = np.random.random(20)
+        heights = heights / heights.sum()
         self.gaussian_means = means.copy()
         self.gaussian_sigmas = sigmas.copy()
         self.gaussian_peak_heights = heights.copy()
         return means, sigmas, heights
 
     def generate_noisy_time_domain(self):
-        sampling_noise = (np.random.random(len(self.t_regular)) - 0.5) * (self.delta / self.samples_per_delta)
+        sampling_noise = (np.random.random(len(self.t_regular)) - 0.5) * (self.t_max / self.n_samples)
         t_domain = self.t_regular + sampling_noise
         self.t_domain = t_domain.copy()
         return t_domain
@@ -109,7 +111,7 @@ class DataGenerator:
         return dst_dir / filename
 
     def get_supersampled_timedomain(self):
-        t_supersampled = np.linspace(0, self.tmax, num=1000)
+        t_supersampled = np.linspace(0, self.t_max, num=1000)
         return t_supersampled
 
     def get_underlying_signal(self, t, image=None, means=None, sigmas=None, heights=None):
@@ -122,7 +124,7 @@ class DataGenerator:
         if image == "A":
             underlying_function = self.gaussian_signal(t, means, sigmas, heights)
         elif image == "B":
-            underlying_function = self.image_ratio * self.gaussian_signal(t + self.delta,
+            underlying_function = self.image_ratio * self.gaussian_signal(t + self.delay,
                                                                           means,
                                                                           sigmas,
                                                                           heights)
@@ -178,12 +180,13 @@ class DataGenerator:
 
         a = self.get_underlying_signal(time_domain, image="A", means=means, sigmas=sigmas, heights=heights)
         b = self.get_underlying_signal(time_domain, image="B", means=means, sigmas=sigmas, heights=heights)
-        sigma_a = np.abs(noise_level * a)
-        sigma_b = np.abs(noise_level * b)
-        noise_a = np.random.normal(0, sigma_a)
-        noise_b = np.random.normal(0, sigma_b)
-        a += noise_a
-        b += noise_b
+        sigma_a = self.relative_err * a
+        sigma_b = self.relative_err * b
+        if self.add_noise:
+            noise_a = np.random.normal(0, sigma_a)
+            noise_b = np.random.normal(0, sigma_b)
+            a += noise_a
+            b += noise_b
 
         return time_domain, a, b, sigma_a, sigma_b
 
@@ -201,9 +204,11 @@ class DataGenerator:
                 n = 500
         return n
 
-    def generate_single_realization_dataset(self, means=None, sigmas=None, heights=None, noise_level=None, gap_size=None):
+    def generate_single_realization_dataset(self, means=None, sigmas=None, heights=None, noise_level=None,
+                                            gap_size=None):
         mask = self.generate_gap_mask(gap_size=gap_size)
-        *data, = self.generate_light_curves(mask=mask, means=means, sigmas=sigmas, heights=heights, noise_level=noise_level)
+        *data, = self.generate_light_curves(mask=mask, means=means, sigmas=sigmas, heights=heights,
+                                            noise_level=noise_level)
         columns = ["time", "A", "B", "sigmaA", "sigmaB"]
         df = pd.DataFrame(data=np.array(data).T, columns=columns)
         return df
@@ -234,7 +239,7 @@ class DataGenerator:
                                                           fname=fname,
                                                           gap_size=gap_size,
                                                           noise_level=noise_level,
-                                                          realization_id=k+1)
+                                                          realization_id=k + 1)
                     dir_path = file_path.parent
                     dir_path.mkdir(parents=True, exist_ok=True)
                     df.to_csv(file_path)
