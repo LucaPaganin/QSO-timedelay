c31e4729fe2151b5f7e1110ee0d9fc55b4de4c47
Luca Paganin <luca.paganin@ge.infn.it>
2020-07-28 22:24:51 +0200

    update notebooks and added mindstep tests

--- modules/utils.py
+++ modules/utils.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import numpy as np
 
 
 def configure_logger(logger, logfile):
@@ -21,4 +22,34 @@ def configure_logger(logger, logfile):
     logger.addHandler(fh)
     logger.addHandler(stdout_handler)
     logger.addHandler(stderr_handler)
+
+def get_season_ends(t):
+    diffs = np.diff(t)
+    season_ends = np.where(diffs >= diffs.mean() + 3*diffs.std())[0]
+    season_ends = np.append(season_ends, len(t)-1)
+    season_ends.sort()
+    
+    return season_ends
+
+def get_season_begs(t):
+    season_ends = get_season_ends(t)
+    season_ends = season_ends[:-1]
+    season_begs = season_ends + 1
+    season_begs = np.append(season_begs, 0)
+    season_begs.sort()
+    
+    return season_begs
+    
+def get_season_masks(t):
+    diffs = np.diff(t)
+    
+    season_ends = get_season_ends(t)
+    season_begs = get_season_begs(t)
+    
+    masks = []
+    
+    for beg, end in zip(season_begs, season_ends):
+        masks.append((t >= t[beg]) & (t <= t[end]))
+    
+    return masks
 
