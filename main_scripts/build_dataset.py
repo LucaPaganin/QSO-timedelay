a1ff5bb4723e4e845ae18291ecadbefdf7182efe
Luca Paganin <luca.paganin@ge.infn.it>
2020-09-21 02:30:14 +0200

    Added log messages

--- main_scripts/build_dataset.py
+++ main_scripts/build_dataset.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import h5py
 import numpy as np
 from pathlib import Path
@@ -31,20 +33,30 @@ def main(args_dict):
     X_data = []
     y_data = []
 
+    qso_id = output_file.stem
+    if qso_id not in input_dir.name:
+        raise Exception(f'Incompatible output file {output_file} and input dir {input_dir}')
+
     for file in files:
+        logger.info(f'Reading file {file}')
         with h5py.File(file, 'r') as hf:
             X_data.append(hf['X'][()])
             y_data.append(hf['y'][()])
 
-    X_data = np.stack(X_data)
-    y_data = np.stack(y_data)
+    X_data = np.concatenate(X_data)
+    y_data = np.concatenate(y_data)
 
     out_file_mode = 'a' if output_file.exists() else 'w'
 
+    logger.info(f'Populating output file {output_file} with training data')
     with h5py.File(output_file, out_file_mode) as out_hf:
-        tr_grp = out_hf.create_group('training_data')
-        tr_grp.create_dataset('X', X_data, **utils.hdf5_opts)
-        tr_grp.create_dataset('y', y_data, **utils.hdf5_opts)
+        if 'training_data' in out_hf:
+            raise Exception(f'File already populated with training data')
+        else:
+            tr_grp = out_hf.create_group('training_data')
+            tr_grp.create_dataset(name='X', data=X_data, **utils.hdf5_opts)
+            tr_grp.create_dataset(name='y', data=y_data, **utils.hdf5_opts)
+    logger.info('Done')
 
 
 if __name__ == '__main__':
